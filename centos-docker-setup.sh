4a887d21d9c1329e5e489d21207f69e212f0e4c9
carvey <carvey@users.noreply.github.com>
2019-02-23 10:16:13 -0500

    Added tmux install and enable docker service on bootup

--- centos-docker-setup.sh
+++ centos-docker-setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-sudo yum install -y vim git
+sudo yum install -y vim git tmux
 wget https://raw.githubusercontent.com/carvey/vim-setup/master/vim-setup
 . vim-setup
 cd ~
+
+echo "set -g mouse-mode on" > ~/.tmux.conf
+echo "set -g mouse-select-pane on" >> ~/.tmux.conf
+echo "set -g mouse-resize-pane on" >> ~/.tmux.conf
 
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum install -y docker-ce docker-ce-cli containerd.io
@@ -14,6 +18,7 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 
 sudo systemctl start docker
+sudo systemctl enable docker
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
